400ca6b8630740fc9bb61c83717d024a495795c0
Nicolás Alberto Gulo <nicogulo@gmail.com>
2024-09-02 18:41:15 -0300

    Edicion casi completa del archivo html, se agrego carpeta js con un script basico que se procedera a editar a continuacion para mayor interactividad

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+var australixElement = document.getElementById('link-sidebar-australix')
+australixElement.addEventListener('click', clicked)
+
+var idSidebarAustralix = idAustralix
+
+function clicked() {
+    const sidebarAustralix = document.getElementById('sidebar-australix')
+    sidebarAustralix.style.display = "block";
+}
+
